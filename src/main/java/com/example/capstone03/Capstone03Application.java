9a601910aa5f9d90d8f7b131206efd7e18ff6b73
Faisal <faisal.a.m.2012@gmail.com>
2025-04-21 19:39:44 +0300

    intial

---
+++ src/main/java/com/example/capstone03/Capstone03Application.java
@@ -0,0 +1,13 @@
+package com.example.capstone03;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Capstone03Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Capstone03Application.class, args);
+    }
+
+}
