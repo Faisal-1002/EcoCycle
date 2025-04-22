72a96cac9110b8e10aebbf80023e4b30d3401408
safcsp abeer <dev@test.com>
2025-04-23 00:56:34 +0300

    add endpoint 11 email notifications

--- src/main/java/com/example/capstone03/Service/PickupRequestService.java
+++ src/main/java/com/example/capstone03/Service/PickupRequestService.java
@@ -3,10 +3,14 @@ package com.example.capstone03.Service;
 import com.example.capstone03.Api.ApiException;
 import com.example.capstone03.Model.ContainerRequest;
 import com.example.capstone03.Model.PickupRequest;
+import com.example.capstone03.Model.RecycleItem;
 import com.example.capstone03.Model.User;
 import com.example.capstone03.Repository.PickupRequestRepository;
+import com.example.capstone03.Repository.RecycleItemRepository;
 import com.example.capstone03.Repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
@@ -17,7 +21,9 @@ import java.util.List;
 public class PickupRequestService {
 
     private final PickupRequestRepository pickupRequestRepository;
+    private final RecycleItemRepository recycleItemRepository;
     private final UserRepository userRepository;
+    private final JavaMailSender mailSender;
 
     public List<PickupRequest> getAllPickupRequests() {
         return pickupRequestRepository.findAll();
@@ -80,5 +86,41 @@ public class PickupRequestService {
     public List<PickupRequest> getAssignedPickupRequests(Integer collectorId) {
         return pickupRequestRepository.findAllByCollectorId(collectorId);
     }
+
+    //endpoint 11- Notify user: Pickup completed + Points updated
+    public String completePickupAndNotify(Integer userId) {
+        User user = userRepository.findUserById(userId);
+        if (user == null) {
+            throw new ApiException("User not found");
+        }
+        List<RecycleItem> items = recycleItemRepository.findRecycleItemsByUserId(userId).stream().filter(item -> item.getPickup_request() != null
+                        && item.getPickup_request().getStatus().equalsIgnoreCase("PickedUp")).toList();
+
+        double totalWeight = items.stream()
+                .mapToDouble(RecycleItem::getWeight_kg).sum();
+
+        int earnedPoints = (int) totalWeight;
+
+        if (earnedPoints == 0) {
+            return "No picked-up items found. No points added.";
+        }
+
+        user.setPoints(user.getPoints() + earnedPoints);
+        userRepository.save(user);
+
+        String message = "Pickup completed!\nYou earned " + earnedPoints + " points.\n\nThank you for recycling with us!";
+        sendEmail(user.getEmail(), " Pickup Completed & Points Earned", message);
+
+        return message;
+    }
+
+    private void sendEmail(String to, String subject, String body) {
+        SimpleMailMessage mail = new SimpleMailMessage();
+        mail.setTo(to);
+        mail.setSubject(subject);
+        mail.setText(body);
+        mailSender.send(mail);
+    }
+
 
 }
