c3d23e93912887d013ae8acbc3d8eb46237a24a2
safcsp abeer <dev@test.com>
2025-04-22 10:30:40 +0300

    add CompanyRequest and Recyclingcompany (Repository Controller and Service) relationship between CompanyRequest and Recyclingcompany

--- src/main/java/com/example/capstone03/Api/ApiResponse.java
+++ src/main/java/com/example/capstone03/Api/ApiResponse.java
@@ -2,10 +2,12 @@ package com.example.capstone03.Api;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.RequiredArgsConstructor;
 
 @Data
 @AllArgsConstructor
 public class ApiResponse {
 
     private String message;
+
 }
